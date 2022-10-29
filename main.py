6c0767737be0fa777e5d479cdd1d388de329981f
student <dimapeduna@gmail.com>
2022-10-29 13:16:01 +0300

    негр негр

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 import random
+class  Humanoid:
+    def __init__(self, name = 'Sim', job = None, home = None, car = None):
+        self.name = name
+        self.job = job
+        self.home = home
+        self.car = car
+        self.glladness = 50
+        self.satiety = 50
